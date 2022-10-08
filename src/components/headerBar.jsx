833df993e165678ecb7ce800a83690975643f498
Awais Ali <awaisalimalik716@gmail.com>
2022-10-08 13:15:11 -0700

    Started Template

---
+++ src/components/headerBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+
+let headerBar = (props) => {
+    return (
+        <div className="image-section-parent">
+            <img src={logo} />
+        </div>
+    )
+}
+
+export default headerBar;
