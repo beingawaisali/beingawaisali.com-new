43a945ec4a3b80aa6a6395bf84cf3d477bf7d04e
Awais Ali <awaisalimalik716@gmail.com>
2022-10-08 14:20:58 -0700

    Installed Font Awesome

--- src/components/headerBar.jsx
+++ src/components/headerBar.jsx
@@ -7,12 +7,14 @@ let HeaderBar = (props) => {
         <div id="main-header">
             <img src={Logo} className="main-logo"/>
             <ul className="manu-bar">
-                <li className="menu-list">About</li>
-                <li className="menu-list">Hire</li>
-                <li className="menu-list">Portfolio</li>
-                <li className="menu-list">Connect</li>
+                <li className="menu-list">01. About</li>
+                <li className="menu-list">02. Hire</li>
+                <li className="menu-list">03 Portfolio</li>
+                <li className="menu-list">04. Connect</li>
                 <li className="menu-list">
-                    <button>Resume</button>
+                    <button>Resume 
+                    <i class="fa-solid fa-arrow-down-to-line"></i>
+                    </button>
                 </li>
             </ul>
         </div>
