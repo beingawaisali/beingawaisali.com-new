7c9eb6dd064f38cd67f6624f699440cd44a3e4c8
Awais Ali <awaisalimalik716@gmail.com>
2022-10-09 04:57:17 -0700

    Resume Button Styled

--- src/components/headerBar.jsx
+++ src/components/headerBar.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './css/header.css';
 import Logo from '../images/Logo-1.png';
 
+let mobileMenu = false;
+
+let toggleMenu = () => {
+    if (mobileMenu == false) {
+        mobileMenu = true
+    } else (
+        mobileMenu = false
+    )
+}
+
 let HeaderBar = (props) => {
     return (
         <div id="main-header">
@@ -12,11 +22,14 @@ let HeaderBar = (props) => {
                 <li className="menu-list">03 Portfolio</li>
                 <li className="menu-list">04. Connect</li>
                 <li className="menu-list">
-                    <button>Resume 
-                    <i class="fa-solid fa-arrow-down-to-line"></i>
+                    <button className="button-1 shape">Resume 
                     </button>
                 </li>
+                
             </ul>
+            <div className="mobile-menu">
+                <i class="fa-light fa-bars"></i>
+            </div>
         </div>
     )
 }
