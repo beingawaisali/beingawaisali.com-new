e2e54e53aec367b257aba9ca10930820de794c61
Awais Ali <awaisalimalik716@gmail.com>
2022-10-09 05:43:08 -0700

    Header Functionality

--- src/components/headerBar.jsx
+++ src/components/headerBar.jsx
@@ -4,25 +4,37 @@ import Logo from '../images/Logo-1.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-let mobileMenu = false;
+// let mobileMenu = false;
 
-let toggleMenu = () => {
-    if (mobileMenu == false) {
-        mobileMenu = true
-    } else (
-        mobileMenu = false
-    )
-}
+// console.log(mobileMenu)
+
+// let toggleMenu = () => {
+//     if (mobileMenu == false) {
+//         mobileMenu = true
+//     } else (
+//         mobileMenu = false
+//     )
+// console.log(mobileMenu)
+// }
+
 
 let HeaderBar = (props) => {
     return (
         <div id="main-header">
             <img src={Logo} className="main-logo"/>
             <ul className="manu-bar">
-                <li className="menu-list">01. About</li>
-                <li className="menu-list">02. Hire</li>
-                <li className="menu-list">03 Portfolio</li>
-                <li className="menu-list">04. Connect</li>
+                <li className="menu-list"><span className="menu-list-num">01. </span>
+                    <a href="#">About</a>
+                </li>
+                <li className="menu-list"><span className="menu-list-num">02. </span>
+                    <a href="#">Hire</a>
+                </li>
+                <li className="menu-list"><span className="menu-list-num">03. </span>
+                    <a href="#">Portfolio</a>
+                </li>
+                <li className="menu-list"><span className="menu-list-num">04. </span>
+                    <a href="#">Connect</a>
+                </li>
                 <li className="menu-list">
                     <button className="button-1 shape">Resume 
                     </button>
@@ -30,7 +42,7 @@ let HeaderBar = (props) => {
                 
             </ul>
             <div className="mobile-menu">
-                <FontAwesomeIcon icon={faBars} />
+                <FontAwesomeIcon icon={faBars} className="menu-icon" onClick={props.toggleMenubar}/>
             </div>
         </div>
     )
